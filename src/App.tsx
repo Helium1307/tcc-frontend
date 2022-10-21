d14048a0f5fae134c7d93b5ae6513aa3cc89cfde
Gabriel Bezerra <gabrieleliasmega@gmail.com>
2022-10-20 21:50:58 -0300

    this commit will add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Routes from "./routes";
+import Sidebar from "./components/sidebar/Sidebar";
+
+
 
 function App() {
   return (
     <div className="App">
       <Routes />
+      <Sidebar/>
     </div>
   );
 }
